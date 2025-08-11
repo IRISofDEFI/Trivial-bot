50cfea0d222dbf381955af360c4e89d6256979b1
IRIS <irisofdefi@gmail.com>
2025-08-11 14:41:00 +0100

    Created a bot called trival

---
+++ scripts.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!")
+
+let botName = "Trival";
+let botLocation = "CodeBase";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+
+let codingFact = "Did you know that " + favoriteLanguage + " " + "Was created in 10 days" + ".";
+console.log(codingFact);
+
+codingFact = " Also when" + " " + favoriteLanguage + " was created i loved it" + ".";
+console.log(codingFact);
+
+codingFact = "Everyone in my family also loved" + " " + favoriteLanguage + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
